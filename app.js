6ea4d2a7f33d3d45001143d57fc962daa06c9b8a
calebfergie <yonohabloespanol@gmail.com>
2020-10-19 22:21:44 -0400

    add working node version

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
-const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+function App() {
+  const [response, setResponse] = useState("");
 
-const app = express();
-app.use(index);
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
 
-const server = http.createServer(app);
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+  );
+}
 
-const io = socketIo(server); // < Interesting!
+export default App;
 
-const getApiAndEmit = "TODO";
+// const express = require("express");
+// const http = require("http");
+// const socketIo = require("socket.io");
+//
+// const port = process.env.PORT || 4001;
+// const index = require("./routes/index");
+//
+// const app = express();
+// app.use(index);
+//
+// const server = http.createServer(app);
+//
+// const io = socketIo(server);
+//
+// let interval;
+//
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
+//   if (interval) {
+//     clearInterval(interval);
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+//
+// const getApiAndEmit = socket => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
+//
+// server.listen(port, () => console.log(`Listening on port ${port}`));
